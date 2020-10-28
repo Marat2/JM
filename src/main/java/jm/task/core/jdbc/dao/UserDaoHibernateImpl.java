62b5c00c94175b1e249b0d44a65223f8654c1de2
Your Name <you@example.com>
2020-10-28 18:55:58 +0500

    change to hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,9 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
-
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
@@ -12,31 +14,66 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        String sql = "CREATE TABLE `user` (`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,`name` VARCHAR(255) NOT NULL, `last_name` VARCHAR(255) NOT NULL, `age` INTEGER (255) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;";
+        session.createNativeQuery(sql).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        String sql = "drop table IF EXISTS user";
+        session.createNativeQuery(sql).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        User user = new User(name,lastName,age);
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        String hql = "delete User where id = :id";
+        Query query = session.createQuery(hql);
+        query.setParameter("id",id);
+        query.executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        String hql = "FROM User";
+        Query query = session.createQuery(hql);
+        List results = query.list();
+        session.getTransaction().commit();
+        session.close();
+        return results;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.getHibernateConnect().openSession();
+        session.beginTransaction();
+        String hql = "delete User";
+        Query query = session.createQuery(hql);
+        query.executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static StandardServiceRegistry registry;
+    public static SessionFactory sessionFactory;
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         String hostName = "localhost";
@@ -15,9 +25,37 @@ public class Util {
         return getMysqlConnect(hostName, dbName, userName, password);
     }
     public static  Connection getMysqlConnect(String hostName, String dbName,String userName, String password)
-            throws SQLException, ClassNotFoundException {
-        //Class.forName("com.mysql.cj.jdbc.Driver");
+            throws SQLException {
         String connectionURL = "jdbc:mysql://localhost:3306/jm?useSSL=false&serverTimezone=UTC";
         return DriverManager.getConnection(connectionURL, userName,password);
+    }
+
+    public static SessionFactory getHibernateConnect(){
+        if (sessionFactory==null){
+            try{
+
+                Properties properties = new Properties();
+
+                properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                properties.setProperty(Environment.USER, "jmuser");
+                properties.setProperty(Environment.PASS, "QazQwe!23456");
+                properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/jm?useSSL=false&serverTimezone=UTC");
+
+                Configuration cfg = new Configuration();
+                cfg.setProperties(properties);
+                cfg.addAnnotatedClass(User.class);
+                sessionFactory = cfg.buildSessionFactory();
+
+            }catch(Exception e){
+                e.printStackTrace();
+
+            }
+        }
+        return sessionFactory;
+    }
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
     }
 }
