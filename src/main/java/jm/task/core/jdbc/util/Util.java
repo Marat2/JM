a8fd76cb291829bb36fe1321193c4d6f0a036fea
Your Name <you@example.com>
2020-10-29 15:08:26 +0500

    change to hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
@@ -32,30 +31,16 @@ public class Util {
 
     public static SessionFactory getHibernateConnect(){
         if (sessionFactory==null){
-            try{
-
                 Properties properties = new Properties();
-
                 properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 properties.setProperty(Environment.USER, "jmuser");
                 properties.setProperty(Environment.PASS, "QazQwe!23456");
                 properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/jm?useSSL=false&serverTimezone=UTC");
-
                 Configuration cfg = new Configuration();
                 cfg.setProperties(properties);
                 cfg.addAnnotatedClass(User.class);
                 sessionFactory = cfg.buildSessionFactory();
-
-            }catch(Exception e){
-                e.printStackTrace();
-
-            }
         }
         return sessionFactory;
-    }
-    public static void shutdown() {
-        if (registry != null) {
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
     }
 }
